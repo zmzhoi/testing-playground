b1817b3e5ad508cdd00dc98e0a8f6b133d68f0cf
zmzhoi <zmzhoi@gmail.com>
2022-09-02 00:24:58 +0900

    chore: 리액트 컴포넌트 테스트 환경 구축
    * eslint 셋팅
    * jest dom 테스트 환경 셋팅
    * testing-library dom 테스트 환경 셋팅

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-  preset: 'ts-jest', // ts-jest가 esm 모듈 구문을 cjs 모듈 구문으로 변환한다.
-  testEnvironment: 'node',
+  preset: 'ts-jest', // jest에서 typescript loader 역할을 함. => ts-jest가 esm 모듈 구문을 cjs 모듈 구문으로 변환한다.
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['./setupTest.ts'],
+  globals: {
+    'ts-jest': {
+      isolatedModules: true, // 타입 체크+트랜스파일링을 하는 타입스크립트의 느린 성능으로, 테스트 속도가 저하 => 이를 해소하는 방법
+    },
+  },
 };
