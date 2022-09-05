1bfe4d3fe27dbb642b5d0a8e4622f25086d06eae
zmzhoi <zmzhoi@gmail.com>
2022-09-05 12:51:34 +0900

    feat: axios 테스트 케이스 추가

--- src/mocking/__tests__/axios.test.ts
+++ src/mocking/__tests__/axios.test.ts
@@ -2,17 +2,42 @@ import axios from 'axios';
 
 import { getUser } from '../user';
 
-it('axios', async () => {
-  axios.get = jest.fn().mockResolvedValue({
-    data: {
-      id: 1,
-      name: 'zmzhoi',
-    },
+describe('axios case 1', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
-  const user = await getUser(1);
-  expect(user).toEqual({
-    id: 1,
-    name: 'zmzhoi',
+  it('axios fetch', async () => {
+    axios.get = jest.fn().mockResolvedValue({
+      data: {
+        id: 1,
+        name: 'zmzhoi',
+      },
+    });
+
+    const user = await getUser(1);
+    expect(user).toEqual({
+      id: 1,
+      name: 'zmzhoi',
+    });
+  });
+
+  it('axios return', async () => {
+    const spyGet = jest.spyOn(axios, 'get');
+
+    spyGet.mockResolvedValue({
+      data: {
+        name: 'amolang',
+      },
+    });
+    const id = 10;
+    const result = await getUser(id);
+
+    expect(axios.get).toHaveBeenCalled();
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveBeenCalledWith(`https://example.com/user/${id}`);
+    expect(result).toEqual({
+      name: 'amolang',
+    });
   });
 });
